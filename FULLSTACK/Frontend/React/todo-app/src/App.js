d2140007c7120653ba54ed08b837bca65132ff34
AnasFaaiz <2310030087@klh.edu.in>
2024-10-21 18:31:37 +0530

    updated

--- FULLSTACK/Frontend/React/todo-app/src/App.js
+++ FULLSTACK/Frontend/React/todo-app/src/App.js
@@ -1,17 +1,32 @@
-
+import { useState, useEffect } from 'react';
 import './App.css';
 import TaskForm from './TaskForm';
 import Tasks from './Tasks';
 
 function App() {
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    const storedTasks = localStorage.getItem('tasks');
+    if (storedTasks) {
+      setTasks(JSON.parse(storedTasks));
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+  }, [tasks]);
+
+  const addTask = (name) => {
+    setTasks((prevTasks) => [...prevTasks, { name, done: false }]);
+  };
+
   return (
     <main>
-     <TaskForm />
-     <Tasks />
-     <Tasks />
-     <Tasks />
-     <Tasks />
-     <Tasks />
+      <TaskForm onAdd={addTask} />
+      {tasks.map((task) => (
+        <Tasks key={task.name} {...task} />
+      ))}
     </main>
   );
 }
--- FULLSTACK/Frontend/React/todo-app/src/TaskForm.js
+++ FULLSTACK/Frontend/React/todo-app/src/TaskForm.js
@@ -1,13 +1,19 @@
 import {useState} from "react";
 
-export default function TaskForm(){
-    const [TaskName, setTaskName] = useState('');
-    return (
-        <form>
-        <button>+</button>
-        <input type="text" value={TaskName}
-            onChange={ev => setTaskName(ev.target.value)}
-            placeholder="New Task...."/>
-      </form>
-    );
+export default function TaskForm({onAdd}) {
+  const [taskName,setTaskName] = useState('');
+  function handleSubmit(ev) {
+    ev.preventDefault();
+    onAdd(taskName);
+    setTaskName('');
+  }
+  return (
+    <form onSubmit={handleSubmit}>
+      <button>+</button>
+      <input type="text"
+             value={taskName}
+             onChange={ev => setTaskName(ev.target.value)}
+             placeholder="Your next task..."/>
+    </form>
+  );
 }
